99aed289b886d9681c258cb94a479a35999da08e
terrencejihoonjung <terrencejung928@gmail.com>
2024-02-19 19:48:25 -0800

    icons and importing

--- learning-angular/src/app/components/task-item/task-item.component.ts
+++ learning-angular/src/app/components/task-item/task-item.component.ts
@@ -1,13 +1,17 @@
 import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { Task } from '../../Task';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-task-item',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FontAwesomeModule],
   templateUrl: './task-item.component.html',
   styleUrls: ['./task-item.component.css'],
 })
 export class TaskItemComponent {
   @Input() task!: Task;
+  faTimes = faTimes;
 }
